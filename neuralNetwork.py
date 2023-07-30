5c92468a99b20022179a69f19a43a36512b0c50c
Khthonian <anansicorp@tuta.io>
2023-07-30 21:14:48 +0100

    Create Neuron class in neuralNetwork.py

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,8 +1,30 @@
 import matplotlib as plt
 import math
 
-# A function for the user to dynamically set the neural network structure
-def getStructure():
-    userInput = input("Enter the structure of the neural network (comma-separated integers): ")
-    structure = [int(neurons) for neurons in userInput.split(",")]
-    return structure
+class Neuron:
+    def __init__(self, nodeNumber, weights, input, output, bias):
+        self.nodeNumber = nodeNumber
+        self.weights = weights
+        self.input = input
+        self.output = output
+        self.error = 0
+        self.bias = bias
+
+    def output(self):
+        net = 0
+        net += self.weights[0] * self.bias
+        for x in range(1, len(self.weights) - 1):
+            net += self.weights[x] * self.input[x]
+        return 1 / (1 + math.exp(-net))
+    
+
+
+# Main
+def main():
+    # Ask the user how many epochs to train for
+    epochs = 0
+    epochMax = int(input("How many epochs should I train for?: "))
+    
+
+if __name__ == "__main__":
+    main()
